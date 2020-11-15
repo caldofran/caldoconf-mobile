5e7a905240fd627390168d360a0a77db4e46f990
Rubén M <ruben.mendez@gmail.com>
2020-11-15 17:33:42 +0100

    Include scroll view in main activity

--- androidApp/src/main/java/com/mobile/caldoconf/androidApp/MainActivity.kt
+++ androidApp/src/main/java/com/mobile/caldoconf/androidApp/MainActivity.kt
@@ -2,7 +2,6 @@ package com.mobile.caldoconf.androidApp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.mobile.caldoconf.shared.Greeting
 import android.widget.TextView
 import com.mobile.caldoconf.shared.ApiClient
 import kotlinx.coroutines.MainScope
@@ -12,18 +11,20 @@ import kotlinx.coroutines.launch
 class MainActivity : AppCompatActivity() {
     private val mainScope = MainScope()
     private val apiClient = ApiClient()
-    private lateinit var textView: TextView
+    private lateinit var informationTitle: TextView
+    private lateinit var informationContent: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        textView = findViewById(R.id.text_view)
+        informationTitle = findViewById(R.id.informationTitle)
+        informationContent = findViewById(R.id.informationContent)
     }
 
     override fun onResume() {
         super.onResume()
-        loadData()
+//        loadData()
     }
 
     override fun onDestroy() {
@@ -36,9 +37,9 @@ class MainActivity : AppCompatActivity() {
             runCatching {
                 apiClient.getEvents()
             }.onSuccess {
-                textView.text = it.toString()
+                informationTitle.text = it.toString()
             }.onFailure {
-                textView.text = it.localizedMessage
+                informationTitle.text = it.localizedMessage
             }
         }
     }
