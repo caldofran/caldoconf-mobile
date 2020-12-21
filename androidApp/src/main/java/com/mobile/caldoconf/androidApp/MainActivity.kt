fd6bb2f50c8602b4638a95f2e37d26431453f28b
Rubén M <ruben.mendez@gmail.com>
2020-12-21 18:31:20 +0100

    Refactor main activity

--- androidApp/src/main/java/com/mobile/caldoconf/androidApp/MainActivity.kt
+++ androidApp/src/main/java/com/mobile/caldoconf/androidApp/MainActivity.kt
@@ -2,30 +2,22 @@ package com.mobile.caldoconf.androidApp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.ScrollView
-import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.mobile.caldoconf.androidApp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ScrollView>
+    private val viewBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val bottomSheetBehavior by lazy { BottomSheetBehavior.from(viewBinding.activityInformation.root) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(viewBinding.root)
 
-        val bottomSheet: ScrollView = findViewById(R.id.activity_information)
-        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
-
-        val buttonBottomSheetPersistent: Button = findViewById(R.id.buttonBottomSheetPersistent)
-        buttonBottomSheetPersistent.setOnClickListener {
-            val state =
-                if (bottomSheetBehavior.state == BottomSheetBehavior.STATE_EXPANDED)
-                    BottomSheetBehavior.STATE_COLLAPSED
-                else
-                    BottomSheetBehavior.STATE_EXPANDED
-            bottomSheetBehavior.state = state
+        viewBinding.buttonBottomSheetPersistent.setOnClickListener {
+            bottomSheetBehavior.state = if (bottomSheetBehavior.state == BottomSheetBehavior.STATE_EXPANDED)
+                BottomSheetBehavior.STATE_COLLAPSED
+            else
+                BottomSheetBehavior.STATE_EXPANDED
         }
     }
 }
